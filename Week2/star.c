65a9900f5a0829aa58c52945585805050d102827
elnazd <elnaz.delpisheh@gmail.com>
2015-09-16 11:35:14 -0400

    it's a starry night

---
+++ Week2/star.c
@@ -0,0 +1,18 @@
+/*Write a C program to create the following output:
+
+It is a starry night!
+*
+**
+***
+****
+*****
+
+*/
+ #include <stdio.h>           // information about the printf identifier 
+
+ int main(void)               // the starting point of the program
+ {
+         printf("It is a starry night!\n*\n**\n***\n****\n*****\n"); // send output to the screen
+
+         return 0;            // return control to the operating system
+ }
