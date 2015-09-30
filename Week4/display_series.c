6ccb0f6a6c19495fef1cbaeec090f0e0a4144252
elnazd <elnaz.delpisheh@gmail.com>
2015-09-30 10:27:52 -0400

    for loop

---
+++ Week4/display_series.c
@@ -0,0 +1,18 @@
+/*Write a program to print all positive numbers until 100 on the screen:
+1,2,3,4...,100
+*/
+#include<stdio.h>
+int main()
+{
+	int counter;
+	
+	//for(set the counter;criteria;increment)
+	for(counter=1;counter<=100;counter++){
+	
+	//actions
+		printf("%d,",counter);
+	
+	}
+
+	return 0;
+}
