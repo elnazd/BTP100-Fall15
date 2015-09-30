9a3988533558e3ed8e8962ace2a9f997e1da1b4f
elnazd <elnaz.delpisheh@gmail.com>
2015-09-30 10:26:04 -0400

    expressions , output

---
+++ Week4/output.c
@@ -0,0 +1,21 @@
+//What is the output of this code?
+#include <stdio.h>
+
+int main()
+{
+	int a = 13;
+	int b = 5;
+
+	printf("%d\n%d\n%d\n%d\n%d\n", a+b, a-b, a*b, a/b, a%b);
+	//output:
+	/*
+		18
+		8
+		65
+		2
+		3
+	
+	*/
+
+	return 0;
+}
