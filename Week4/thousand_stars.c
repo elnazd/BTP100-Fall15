73aa6b209ac0af0f13cc9d242f6965c3c6733efa
elnazd <elnaz.delpisheh@gmail.com>
2015-09-30 10:25:04 -0400

    While loop

---
+++ Week4/thousand_stars.c
@@ -0,0 +1,16 @@
+/*Write a program to print  1000 stars onto the display.*/
+#include<stdio.h>
+int main()
+{
+	int counter=0;
+	while(counter<1000){
+		
+		//action
+		printf("*");
+		counter = counter+1; //counter++;	
+	
+	}
+
+
+	return 0;
+}
